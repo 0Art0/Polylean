c5a8fc581168779080353be5bd5d1aae8d6052f4
ART0 <anand.rao.art@gmail.com>
2022-02-03 23:04:53 +0530

    Add proof of minimality

--- Polylean/WatsonCrickLength.lean
+++ Polylean/WatsonCrickLength.lean
@@ -31,6 +31,8 @@ A total order on the matchings of a word in terms of the "energy" (the number of
 instance {w : Word α} : LE (MatchWord w) :=
  ⟨λ m m' => unmatchedCount m ≤ unmatchedCount m'⟩
 
+instance decideMatchWord {w : Word α}: DecidableEq (MatchWord w) := by sorry
+
 instance {w : Word α} : ∀ m m' : MatchWord w, Decidable (m ≤ m') := by
   intro ; intro ; simp [LE.le] ; exact inferInstance
 
@@ -69,11 +71,28 @@ The definition of Watson-Crick length, as the number of unmatched letters in a m
 def WatsonCrickLength : FreeGroupLength α :=
   λ w => unmatchedCount $ maxMatch w
 
+def ConjugateSplit.toMatchWord {w : Word α} (matchFun : (w : Word α) → MatchWord w) : ConjugateSplit w → MatchWord w
+  | ConjugateSplit.split l fst snd => MatchWord.matching l (matchFun fst) (matchFun snd)
+
+/-
+The above `maxMatch` function does not properly reduce, due to the clutter required to prove termination.
+This function implements the same code, in a way that is transparent and suitable for theorem proving.
+Ideally this function should not be required.
+-/
+def maxMatch' : (w : Word α) → MatchWord w
+  | List.nil => MatchWord.nil
+  | List.cons l w' =>
+    let base := MatchWord.cons l (maxMatch w')
+    let splits := (Word.conjugateSplits (l :: w')).map (ConjugateSplit.toMatchWord maxMatch)
+    List.foldl min base splits
+
+-- a theorem stating the equivalence of the two functions
+theorem maxMatch'equiv : ∀ w : Word α, maxMatch w = maxMatch' w := by admit
 
 /-
 If `m` is a matching of a word `w`, the length of `w` under any pseudo-length function is
-bounded by the "energy" of `m` (the number of unmatched letters).
-
+bounded by the "energy" of `m` (the number of unmatched letters).-/
+/-
 This theorem is crucial for proving the maximality of the Watson-Crick length.
 -/
 theorem unmatchedCountBound {w : Word α} (m : MatchWord w) :
@@ -96,8 +115,34 @@ theorem unmatchedCountBound {w : Word α} (m : MatchWord w) :
 A proof that the output of `maxMatch` has the least number of unmatched letters among all possible matchings of a word.
 -/
 theorem maximalMatching {w : Word α} : (m : MatchWord w) → (maxMatch w) ≤ m
-  | nil => sorry
-  | cons l m' => sorry
-  | matching l m₁ m₂ => sorry
+  | nil => by
+    rw [maxMatch'equiv]
+    simp [maxMatch', LE.le]
+    apply Nat.le_refl
+  | cons l m' => by
+    rw [maxMatch'equiv]
+    simp [maxMatch']
+    apply TotalOrder.transLE
+    · apply List.foldMinBase
+    · simp [LE.le]
+      apply Nat.succ_le_succ
+      apply (maximalMatching m')
+  | matching l m₁ m₂ => by
+    rename_i fst snd
+    apply TotalOrder.transLE
+    · show (maxMatch (l :: (fst ++ [l⁻¹] ++ snd))) ≤ matching l (maxMatch fst) (maxMatch snd)
+      rw [maxMatch'equiv]
+      simp [maxMatch']
+      apply List.foldMinMem
+      let cs := ConjugateSplit.split l fst snd
+      have : matching l (maxMatch fst) (maxMatch snd) = ConjugateSplit.toMatchWord maxMatch cs := rfl
+      rw [this]
+      apply List.mapMem
+      apply ConjugateSplit.allSplits
+    · show (matching l (maxMatch fst) (maxMatch snd)) ≤ matching l m₁ m₂
+      simp [LE.le] at *
+      apply Nat.add_le_add
+      · apply (maximalMatching m₁)
+      · apply (maximalMatching m₂)
 
 end MatchWord
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ A replication in __lean 4__ of some of the __scala__ code used in the _PolyMath 
 
 This fork of the repository is an attempt to understand and experiment with the approach of producing computer-generated proofs for length bounds on words in a free group.
 
+It contains a formalisation of the result from the paper [Random words in free groups, non-crossing matches and RNA secondary structures](https://arxiv.org/pdf/2007.12109.pdf), concerning the minimality of a certain length function known as the "Watson-Crick length". 
+
 ---
